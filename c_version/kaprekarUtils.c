f6d2c635250e7264d98a6922f7d0523c9ac5d799
Jarrod Daniel Blaser <jblaser@cse.unl.edu>
2018-09-05 20:20:56 -0500

    Fixed errors I made

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -36,8 +36,7 @@ int isKaprekar(int n) {
 
 
   //for each possible "split" of the square...
-  int numberOfDigits;
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
     modulus *= 10;
 
